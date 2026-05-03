db386971d1e176b3993b568de6b4581767fc5b8f
Kalp Shah <74227865+kalpshah18@users.noreply.github.com>
2026-05-04 00:49:14 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        string t = "";
+        for(int i = 0; i < n; i++){
+            if(isalnum(s[i])) t += tolower(s[i]);
+        }
+        cout << t << endl;
+        string k = t;
+        reverse(k.begin(), k.end());
+        for(int i = 0; i < t.size(); i++) if(t[i] != k[i]) return false;
+        return true;
+    }
+};
